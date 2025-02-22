ecd9dea18c700b974d16b73c48a7d3ccc66d10e1
FinnSetchell <finn@setchell.org>
2025-02-22 16:39:27 +0100

    fixed for neoforge 21.4.86+

--- neoforge/src/main/java/com/finndog/mns/datagen/StructureNbtUpdaterDatagen.java
+++ neoforge/src/main/java/com/finndog/mns/datagen/StructureNbtUpdaterDatagen.java
@@ -2,9 +2,10 @@ package com.finndog.mns.datagen;
 
 import com.finndog.mns.MNSCommon;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 // Source: https://github.com/BluSunrize/ImmersiveEngineering/blob/1.20.1/src/datagen/java/blusunrize/immersiveengineering/data/IEDataGenerator.java
@@ -13,11 +14,10 @@ public class StructureNbtUpdaterDatagen {
 
     @SubscribeEvent
     public static void gatherData(GatherDataEvent.Server event) {
-        ExistingFileHelper exHelper = event.getExistingFileHelper();
+        ResourceManager resourceManager = event.getResourceManager(PackType.SERVER_DATA);
         DataGenerator gen = event.getGenerator();
         final var output = gen.getPackOutput();
-
-        gen.addProvider(true, new StructureNbtUpdater("structures", MNSCommon.MODID, exHelper, output));
+        gen.addProvider(true, new StructureNbtUpdater("structure", MNSCommon.MODID, resourceManager, output));
 
     }
 }
